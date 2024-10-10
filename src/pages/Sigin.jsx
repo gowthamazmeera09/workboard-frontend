683e2ea9441d75881f47700d4b53f134ec4faa3a
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-10 22:51:20 +0530

    just checking the dark mode

--- src/pages/Sigin.jsx
+++ src/pages/Sigin.jsx
@@ -4,7 +4,7 @@ function Sigin() {
   return (
     <form class="max-w-sm mx-auto mt-20 lg:mt-[-40px]">
     <div class="mb-5">
-      <label for="email" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-white">Your email</label>
+      <label for="email" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Your email</label>
       <input type="email" id="email" class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
     </div>
     <div class="mb-5">
