f420d931a1cb6bdb692c94cd44c91c71656fad6b
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-10 22:45:16 +0530

    a slight changes from the above commit

--- src/pages/Sigup.jsx
+++ src/pages/Sigup.jsx
@@ -57,22 +57,22 @@ function Signup() {
     <form className=" max-w-md  mx-auto mt-20 lg:mt-[-100px]  " onSubmit={handlesubmit}>
       
       <div className="mb-5">
-        <label htmlFor="username" className="block mb-2 text-sm font-medium mx-24 text-gray-900 dark:text-white">Username</label>
-        <input type="text" name='username' value={username} onChange={(e) => setUserName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-24 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
+        <label htmlFor="username" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-white">Username</label>
+        <input type="text" name='username' value={username} onChange={(e) => setUserName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-16 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
       </div>
       <div className="mb-5">
-        <label htmlFor="email" className="block mb-2 text-sm font-medium mx-24 text-gray-900 dark:text-white">Your email</label>
-        <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-24 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+        <label htmlFor="email" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-white">Your email</label>
+        <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-16 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
       </div>
       <div className="mb-5">
-        <label htmlFor="password" className="block mb-2 text-sm font-medium mx-24 text-gray-900 dark:text-white">Create a password</label>
-        <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-24 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        <label htmlFor="password" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-white">Create a password</label>
+        <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-16 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
       </div>
       <div className="mb-5">
-        <label htmlFor="phonenumber" className="block mb-2 text-sm font-medium mx-24 text-gray-900 dark:text-white">Phone Number</label>
-        <input type="number" name='phonenumber' value={phonenumber} onChange={(e) => setPhoneNumber(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-24 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="970*******" required />
+        <label htmlFor="phonenumber" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-white">Phone Number</label>
+        <input type="number" name='phonenumber' value={phonenumber} onChange={(e) => setPhoneNumber(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-16 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="970*******" required />
       </div>
-      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 mx-24 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-small sm:w-auto px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
+      <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 mx-16 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-small sm:w-auto px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
     </form>
   );
 }
