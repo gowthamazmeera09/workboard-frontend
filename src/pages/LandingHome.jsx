77bf979bb189ad1cb075b595e283dfc628fa9035
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-11 17:16:08 +0530

    added a login page at home page

--- src/pages/LandingHome.jsx
+++ src/pages/LandingHome.jsx
@@ -1,14 +1,39 @@
 import React from 'react'
 import welcomeimage from '../images/workboard-main-image-removebg.png';
-import { TbMarquee } from 'react-icons/tb';
+import { Link } from 'react-router-dom';
+
 
 function LandingHome() {
   return (
     <div>
-        
-        <div className="flex items-center justify-center my-20 lg:mt-[-100px]">
-        <img src={welcomeimage } width='400px' className="welcome-image" />
+      <div className="flex items-center justify-center my-20 lg:mt-[-100px]">
+        <img src={welcomeimage} width='400px' className="welcome-image" />
+        <div className='hidden lg:block'>
+          <form class="max-w-sm mx-auto mt-20 lg:mt-[-40px]">
+            <div class="mb-5">
+              <label for="email" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Your email</label>
+              <input type="email" id="email" class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+            </div>
+            <div class="mb-5">
+              <label for="password" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Your password</label>
+              <input type="password" id="password" class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+            <div className='mb-5'>
+              <label for="" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Don't hava an account?
+                <Link to="/Sigup" className="text-sm text-blue-800  dark:text-blue-500 hover:underline"> Register here</Link>
+              </label>
+            </div>
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 mx-16 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-small sm:w-auto px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+          </form>
+        </div>
       </div>
+      <div className='mt-[-85px] lg:hidden'>
+          <p className='text-3xl'>Hello,<br></br>How is your day</p>
+          <br/>
+          <p>Please continue to <Link to="/Sigin" className="text-sm text-blue-800  dark:text-blue-500 hover:underline"> Login</Link></p>
+        </div>
+
+
     </div>
   )
 }
--- src/pages/Sigup.jsx
+++ src/pages/Sigup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { API_URL } from '../data/data';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Signup() {
   const [username, setUserName] = useState("");
@@ -46,7 +46,7 @@ function Signup() {
         setEmail("");
         setPassword("");
       }
-      
+
     } catch (error) {
       console.log(error);
       alert("Something went wrong");
@@ -55,7 +55,7 @@ function Signup() {
 
   return (
     <form className=" max-w-md  mx-auto mt-20 lg:mt-[-100px]  " onSubmit={handlesubmit}>
-      
+
       <div className="mb-5">
         <label htmlFor="username" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Username</label>
         <input type="text" name='username' value={username} onChange={(e) => setUserName(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-16 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
@@ -71,6 +71,12 @@ function Signup() {
       <div className="mb-5">
         <label htmlFor="phonenumber" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Phone Number</label>
         <input type="number" name='phonenumber' value={phonenumber} onChange={(e) => setPhoneNumber(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg w-60 mx-16 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="970*******" required />
+      </div>
+      <div className='mb-5'>
+      <label for="password" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Already hava an account!
+        <Link to="/Sigin" className="text-sm text-blue-800  dark:text-blue-500 hover:underline"> Login</Link>
+        </label>
+      
       </div>
       <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 mx-16 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-small sm:w-auto px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
     </form>
