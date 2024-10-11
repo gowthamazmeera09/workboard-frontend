639b05203707f08f59359c417f5f1a8e1c6ca81f
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-11 10:50:56 +0530

    dfijvdfvbdff

--- src/pages/LandingHome.jsx
+++ src/pages/LandingHome.jsx
@@ -5,8 +5,9 @@ import { TbMarquee } from 'react-icons/tb';
 function LandingHome() {
   return (
     <div>
-        <div className="flex items-center justify-center my-20">
-        <img src={welcomeimage } width='300px' className="welcome-image" />
+        
+        <div className="flex items-center justify-center my-20 lg:mt-[-100px]">
+        <img src={welcomeimage } width='400px' className="welcome-image" />
       </div>
     </div>
   )
