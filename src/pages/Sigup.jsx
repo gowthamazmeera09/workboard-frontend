e41e95484f532a4a5a4db781a5b3eaf77e305134
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-22 10:51:20 +0530

    sdhuvudbud

--- src/pages/Sigup.jsx
+++ src/pages/Sigup.jsx
@@ -1,95 +1,118 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { API_URL } from '../data/data';
+import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../data/data';
+// import { useDispatch } from 'react-redux';
+// import { login } from '../redux/userSlice';
 
 
 function Sigup() {
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-    phonenumber: '',
-  });
-  const [image, setImage] = useState(null);
-  const [success, setSuccess] = useState('');
-  const [error, setError] = useState('');
-  const Navigate = useNavigate();
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+    const [username,setUserName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [phonenumber,setPhoneNumber] = useState("");
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [error,setError] = useState(null);
+    const [loading,setLoading] = useState(false);
+    const navigate = useNavigate();
+   
 
-  // Handle image file input change
-  const handleImageChange = (e) => {
-    setImage(e.target.files[0]);
-  };
 
-  // Submit form
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+    
 
-    // Prepare form data for sending
-    const form = new FormData();
-    form.append('username', formData.username);
-    form.append('email', formData.email);
-    form.append('password', formData.password);
-    form.append('phonenumber', formData.phonenumber);
-    form.append('image', image);
+    const handlesubmit = async(e)=>{
+        e.preventDefault();
+        try {
+            setLoading(true);
+            const formData = new FormData();
+        formData.append('username', username);
+        formData.append('email', email);
+        formData.append('password', password);
+        formData.append('phonenumber', phonenumber);
+        if (selectedFile) {
+            formData.append('profilePicture', selectedFile); // Use 'profilePicture' as the field name
+        }
 
-    try {
-      const response = await axios.post(`${API_URL}user/register`, form, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-      });
+        
+        const response = await fetch(`${API_URL}user/register`,{
+            method: 'POST',
+            body: formData // No need for 'Content-Type' header when using FormData
+        });
+            const data = await response.json();
+            if(response.ok){
+                alert("registation successfull");
+                console.log(data);
+                if (data.user.profilePicture) {
+                    localStorage.setItem('profilePicture', `${API_URL}${data.user.profilePicture}`);
+                }
+                navigate('/Sigin')
+            }
+            else if(email){
+                alert("email already taken");
+                setUserName("");
+                setEmail("");
+                setPassword("");
+              }
+              else {
+                setUserName("");
+                setEmail("");
+                setPassword("");
+              }
+              setLoading(false);
+              setError(false);
 
-      setSuccess(response.data.success);
-      Navigate('/Verificationpage')
-      setError('');
-    } catch (error) {
-      console.error(error)
-      setSuccess('');
+        } catch (error) {
+            console.error(error);
+            alert('registation faled');
+            setLoading(false);
+            setError(true);
+
+        }
     }
-  };
-  return (
-    <div>
-    {success && <div style={{ color: 'green' }}>{success}</div>}
-    {error && <div style={{ color: 'red' }}>{error}</div>}
-    <form class="max-w-sm mx-auto mt-20 lg:mt-[-120px]" onSubmit={handleSubmit}>
-    <div class="mb-5">
-        <label for="username" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">username</label>
-        <input type="text" name='username' value={formData.username} onChange={handleChange}  class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-      </div>
-      <div class="mb-5">
-        <label for="email" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Your email</label>
-        <input type="email" name='email' value={formData.email} onChange={handleChange}  class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-      </div>
-      <div class="mb-5">
-        <label for="password" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">create a password</label>
-        <input type="password" name='password' value={formData.password} onChange={handleChange}  class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-      </div>
-      <div class="mb-5">
-        <label for="phonenumber" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">your phonenumber</label>
-        <input type="number" name='phonenumber' value={formData.phonenumber}  onChange={handleChange} class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-      </div>
-      <div class="mb-5">
-        <label for="image" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">profile image</label>
-        <input type="file"   onChange={handleImageChange} class="bg-gray-50 border border-gray-300 mx-16 text-gray-900 text-sm rounded-lg  w-60 focus:ring-blue-500 focus:border-blue-500 block w-medium lg:w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-      </div>
-      
-      <div className='mb-5'>
-      <label for="password" class="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Already hava an account?
-        <Link to="/Sigin" className="text-sm text-blue-800  dark:text-blue-500 hover:underline"> Login here</Link>
-        </label>
-      
-      </div>
-      
+    return (
+        <div>
 
-      <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 mx-16 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-small sm:w-auto px-5 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register</button>
-    </form>
-  </div>
-  );
+
+
+            <form class="max-w-sm mx-auto" onSubmit={handlesubmit}>
+            <div class="mb-5">
+
+                
+                    <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">username</label>
+                    <input type="text" name='username' value={username} onChange={(e)=>setUserName(e.target.value)}   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="username" required />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                    <input type="email" name='email' value={email} onChange={(e)=>setEmail(e.target.value)}   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
+                    <input type="password" name='password' value={password} onChange={(e)=>setPassword(e.target.value)}   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="your password"  required />
+                </div>
+                <div class="mb-5">
+                    <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">phonenumber</label>
+                    <input type="phone" name='phonenumber' value={phonenumber} onChange={(e)=>setPhoneNumber(e.target.value)}   class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="970*******" required />
+                </div>
+
+                <div className="mb-5">
+                    <label htmlFor="profilePicture" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Profile Picture</label>
+                    <input type="file" name="profilePicture" accept="image/*" onChange={(e) => setSelectedFile(e.target.files[0])}  className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" />
+                </div>
+                
+                
+                
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{ loading? 'Loading...':'Sigup' }</button>
+
+                <div className='flex'>
+                    <p> have an account?</p>
+                    <Link to="/Sigin" >
+                    <span className='text-blue-600'>Sigin</span>
+                    </Link>
+                </div>
+
+            </form>
+
+        </div>
+    )
 }
 
 export default Sigup;
