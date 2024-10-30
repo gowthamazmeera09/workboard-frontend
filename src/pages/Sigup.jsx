2ab4278b4ce8b901115182bcdf5b3d50477233ac
gowthamazmeera09 <gowthamazmeera@gmail.com>
2024-10-30 14:40:03 +0530

    shubvufvbfygfsdbvysgvuhfg7y

--- src/pages/Sigup.jsx
+++ src/pages/Sigup.jsx
@@ -66,23 +66,23 @@ function Sigup() {
       {error && <div style={{ color: 'red' }}>{error}</div>}
       <form class="max-w-sm mx-auto mt-20 lg:mt-[-300px]" onSubmit={handleSubmit}>
         <div class="mb-5">
-          <label for="username"className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">username</label>
+          <label for="username"className="block mb-2 text-sm font-medium  text-gray-900 dark:text-black">username</label>
           <input type="text" name='username' value={username} onChange={(e) => setUsername(e.target.value)} class="shadow-sm bg-gray-50 border h-8 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
         </div>
         <div class="mb-5">
-          <label for="email" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Your email</label>
+          <label for="email" className="block mb-2 text-sm font-medium  text-gray-900 dark:text-black">Your email</label>
           <input type="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} class="shadow-sm bg-gray-50 border border-gray-300 h-8 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
         </div>
         <div class="mb-5">
-          <label for="password" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">create a password</label>
+          <label for="password" className="block mb-2 text-sm font-medium  text-gray-900 dark:text-black">create a password</label>
           <input type="password" name='password' value={password} onChange={(e) => setPassword(e.target.value)} class="shadow-sm bg-gray-50 border h-8 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
         </div>
         <div class="mb-5">
-          <label for="phonenumber" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">your phonenumber</label>
+          <label for="phonenumber" className="block mb-2 text-sm font-medium  text-gray-900 dark:text-black">your phonenumber</label>
           <input type="number" name='phonenumber' value={phonenumber} onChange={(e) => setPhonenumber(e.target.value)} class="shadow-sm bg-gray-50 border h-8 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
         </div>
         <div class="mb-5">
-          <label htmlFor="photo" className="block mb-2 text-sm font-medium mx-16 text-gray-900 dark:text-black">Upload Photo</label>
+          <label htmlFor="photo" className="block mb-2 text-sm font-medium  text-gray-900 dark:text-black">Upload Photo</label>
           <input type="file" name="photo" onChange={(e) => setFile(e.target.files[0])} className="block w-full" />
         </div>
 
