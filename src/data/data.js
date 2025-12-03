dbdba781ed2279f8965df57f9737091f4dd86fd1
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-03 23:00:23 +0530

    klklklk

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,5 @@
-// export const API_URL = "http://localhost:2000/"
+export const API_URL = "http://localhost:2000/"
 
-export const API_URL = "https://workboard-backend.onrender.com/"
+// export const API_URL = "https://workboard-backend.onrender.com/"
 
 
