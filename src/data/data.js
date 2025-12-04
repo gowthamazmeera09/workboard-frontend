12b7561ad0a4a66695e6f206b59beba015559346
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-04 11:08:21 +0530

    ghjkghjk

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
- // export const API_URL = "http://localhost:2000/"
+export const API_URL = "http://localhost:2000/"
 
-export const API_URL = "https://workboard-backend.onrender.com/"
+// export const API_URL = "https://workboard-backend.onrender.com/"
 
 // export const API_URL = "https://workboard-backend-production.up.railway.app/"
 
