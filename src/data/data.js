dc48fa572b90533a24033d2e1508debcedeb1430
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-04 11:55:50 +0530

    hjhghghhg

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,7 @@
 export const API_URL = "http://localhost:2000/"
 
+// export const API_URL = "http://192.168.2.252:2000/"
+
 // export const API_URL = "https://workboard-backend.onrender.com/"
 
 // export const API_URL = "https://workboard-backend-production.up.railway.app/"
