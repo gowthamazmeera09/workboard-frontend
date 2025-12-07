533aa8b3dad8e420f7450147e3bfef6bf29c8305
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-07 11:16:31 +0530

    added railway server

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,8 @@
-export const API_URL = "http://localhost:2000/"
+// export const API_URL = "http://localhost:2000/"
 
 
 // export const API_URL = "https://workboard-backend.onrender.com/"
 
-// export const API_URL = "https://workboard-backend-production.up.railway.app/"
+export const API_URL = "https://workboard-backend-production.up.railway.app/"
 
 
