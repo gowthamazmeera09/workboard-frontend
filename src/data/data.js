ed960fbd61e320f81d3ca430fe06c3dd992d8b9a
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-09 16:02:58 +0530

    Update API_URL to new Cloudflare endpoint

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,9 @@
 // export const API_URL = "http://localhost:2000/"
 
+export const API_URL = "https://vessel-spokesman-genius-fishing.trycloudflare.com/"
 
-export const API_URL = "https://workboard-backend.onrender.com/"
+
+// export const API_URL = "https://workboard-backend.onrender.com/"
 
 // export const API_URL = "https://workboard-backend-production.up.railway.app/"
 
