479bc5bfa450a6a968c6bb79de09a1c8cd8b69d3
gowthamazmeera09 <gowthamazmeera@gmail.com>
2025-12-10 11:18:10 +0530

    Update API_URL to new endpoint

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // export const API_URL = "http://localhost:2000/"
 
-export const API_URL = "https://vessel-spokesman-genius-fishing.trycloudflare.com/"
+export const API_URL = "https://stories-nhs-predicted-seq.trycloudflare.com/"
 
 
 // export const API_URL = "https://workboard-backend.onrender.com/"
